f5e23b8658a795c7b4d43ebcfdd0985e5c8a981d
shani patel <shanibpatel@outlook.com>
2024-04-21 17:24:30 +0530

    Added Report and Code Cleanup

--- WPFThermalPrint/MainWindow.xaml.cs
+++ WPFThermalPrint/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System.Drawing.Printing;
 using System.IO;
 using System.Windows;
-using System.Windows.Controls;
 
 namespace WPFThermalPrint
 {
--- WPF_Reports/WPF_Reports/MainWindow.xaml.cs
+++ WPF_Reports/WPF_Reports/MainWindow.xaml.cs
@@ -1,13 +1,8 @@
-﻿using System.Text;
+﻿using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPF_Reports
 {
@@ -16,9 +11,68 @@ namespace WPF_Reports
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Font printFont;
+        private StreamReader streamToPrint;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void PrintButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                streamToPrint = new StreamReader
+                   ("C:\\Reports\\Test.txt");
+                try
+                {
+                    printFont = new Font("Arial", 10);
+                    PrintDocument pd = new PrintDocument();
+                    pd.PrintPage += new PrintPageEventHandler
+                       (this.pd_PrintPage);
+                    pd.Print();
+                }
+                finally
+                {
+                    streamToPrint.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void pd_PrintPage(object sender, PrintPageEventArgs ev)
+        {
+            float linesPerPage = 0;
+            float yPos = 0;
+            int count = 0;
+            float leftMargin = ev.MarginBounds.Left;
+            float topMargin = ev.MarginBounds.Top;
+            string line = null;
+
+            // Calculate the number of lines per page.
+            linesPerPage = ev.MarginBounds.Height /
+               printFont.GetHeight(ev.Graphics);
+
+            // Print each line of the file.
+            while (count < linesPerPage &&
+                              ((line = streamToPrint.ReadLine()) != null))
+            {
+                yPos = topMargin + (count *
+                                      printFont.GetHeight(ev.Graphics));
+                ev.Graphics.DrawString(line, printFont, Brushes.Black,
+                                      leftMargin, yPos, new StringFormat());
+                count++;
+            }
+
+            // If more lines exist, print another page.
+            if (line != null)
+                ev.HasMorePages = true;
+            else
+                ev.HasMorePages = false;
         }
     }
 }
